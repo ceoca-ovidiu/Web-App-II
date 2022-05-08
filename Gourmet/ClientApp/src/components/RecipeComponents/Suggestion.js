21cf0fb1febd557cd8f98074c82cbea2bbfc7761
sergiumatei03 <sergiu.matei03@yahoo.com>
2022-05-09 01:06:54 +0300

    Home page design

---
+++ Gourmet/ClientApp/src/components/RecipeComponents/Suggestion.js
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import "../../styles/RecipeCard.css";
+
+export default function Recipe(props) {
+    const text = props.text;
+    return (
+        <div className="card-suggestion">
+            <p>
+                Te-ai întrebat vreodată care sunt originile îndrăgitului preparat numit musaca? Delicioasă şi apetisantă, musacaua este o marcă a bucătăriei greceşti.
+                Musacaua este o mâncare preparată din carne tocată şi felii de vinete, aşezate în straturi alternative, acoperite cu un strat gros de sos béchamel, care este coaptă în cuptor şi astfel devine crocantă şi rumenită frumos.
+            </p>
+        </div>
+    );
+}
