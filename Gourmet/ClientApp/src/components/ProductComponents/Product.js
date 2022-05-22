8f5f87a4b9e5a0ce35284bde6fd2467be8739a7e
enyediervin <ervinfcb@gmail.com>
2022-05-22 20:29:49 +0300

    Style changes and Cart error handling.

--- Gourmet/ClientApp/src/components/ProductComponents/Product.js
+++ Gourmet/ClientApp/src/components/ProductComponents/Product.js
@@ -11,31 +11,23 @@ export default function Product(props) {
   function onClickHandle() {
     // TODO: Add to cart => create global cart storage
     let productList = sessionStorage.getItem("productList");
-    productList === ""
-      ? (productList = [])
-      : (productList = JSON.parse(productList));
+    if (productList == null || productList === "") {
+      productList = [];
+    } else {
+      productList = JSON.parse(productList);
+    }
     productList.push(props);
     sessionStorage.setItem("productList", JSON.stringify(productList));
     alert("Item has been added to te cart");
   }
-/*
-  function _base64ToArrayBuffer(base64) {
-    var binary_string = window.atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-      bytes[i] = binary_string.charCodeAt(i);
-    }
-    return bytes.buffer;
-  }
-  */
+
   return (
     <div className="card-container">
-          <img className="card-image" src={imageSource} />
-      <h5 className="card-subtitle">{name}</h5>
-      <h6 className="card-subtitle">Descriere</h6>
+      <img className="card-image" src={imageSource} />
+      <h4 className="card-subtitle">{name}</h4>
+      <h5 className="card-subtitle">Descriere</h5>
       <p>{text}</p>
-      <h5 className="card-subtitle">
+      <h5 className="card-subtitle-price">
         Cost: {price} Ron{unit ? "/" + unit : null}
       </h5>
       <button className="card-addButton" onClick={onClickHandle}>
